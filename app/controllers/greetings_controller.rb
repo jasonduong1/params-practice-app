21a7b21306f6b66cf4a5e1674f963cd0d45d96e0
jason <jduong89@gmail.com>
2022-03-12 22:24:40 -0800

    adds home path and home and hello_bye actions

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,2 +1,16 @@
 class GreetingsController < ApplicationController
+  def home
+    render json: { message: "home page" }
+  end
+
+  def hello_bye
+    input = params[:id]
+    if input == "hello"
+      render json: { message: "HELLO!" }
+    elsif input == "bye"
+      render json: { message: "GOODBYE!" }
+    else
+      render json: { message: "123" }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/home" => "greetings#home"
+  get "/home/:id" => "greetings#hello_bye"
 end
